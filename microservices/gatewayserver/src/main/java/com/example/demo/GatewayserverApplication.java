624211c5ae2b04928dbc25ddfc07eba6d6c9925c
galihd <galihdiarsyah@gmail.com>
2020-10-14 19:23:09 +0700

    update

---
+++ microservices/gatewayserver/src/main/java/com/example/demo/GatewayserverApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+	
+	@Bean
+	public RouteLocator routelocator(RouteLocatorBuilder builder) {
+		return builder.routes().build();
+	}
+
+}
