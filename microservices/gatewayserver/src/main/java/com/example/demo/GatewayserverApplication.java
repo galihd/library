49005697007e813a865f91f781e81de10c21f292
galihd <galihdiarsyah@gmail.com>
2020-11-02 01:01:15 +0700

    update

--- microservices/gatewayserver/src/main/java/com/example/demo/GatewayserverApplication.java
+++ microservices/gatewayserver/src/main/java/com/example/demo/GatewayserverApplication.java
@@ -6,11 +6,18 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RestController
 public class GatewayserverApplication {
 
+	@GetMapping(value = "/")
+	public String HelloClient (){
+		return "Hello Client";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
