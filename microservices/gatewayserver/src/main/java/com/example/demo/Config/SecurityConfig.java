2a6f1a5584655a8021b31e9fd7be7b760a68811a
galihd <galihdiarsyah@gmail.com>
2020-11-20 05:21:48 +0700

    update route

--- microservices/gatewayserver/src/main/java/com/example/demo/Config/SecurityConfig.java
+++ microservices/gatewayserver/src/main/java/com/example/demo/Config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.example.demo.Config;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
@@ -9,15 +7,10 @@ import org.springframework.security.authentication.ReactiveAuthenticationManager
 import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-
-import reactor.core.publisher.Mono;
 
 
 @EnableWebFluxSecurity
